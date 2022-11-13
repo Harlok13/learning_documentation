30a32af0bac04c33f5a42c25c652f6a6ccf42e24
Harlok <holmik90@yandex.ru>
2022-11-13 21:14:26 +0300

    add index, detail, results, vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,21 @@
 # Здесь находятся сами представления
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('hello, world. You\'re at the polls index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse(f'You\'re looking at the question {question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'You\'re looking at the results of the question {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}')
